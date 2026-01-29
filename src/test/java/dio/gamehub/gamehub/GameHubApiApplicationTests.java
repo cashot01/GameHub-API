6525afde58f3d5172dd6fbf364b4f16a4bb5f763
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2026-01-29 10:16:48 -0300

    Initial commit

---
+++ src/test/java/dio/gamehub/gamehub/GameHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.gamehub.gamehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
